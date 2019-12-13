2225e8c3c9720948486b105bb5c882e9737800aa
perismemo <pmemoi2015@gmail.com>
2019-12-13 14:32:08 +0300

    web application

---
+++ index.php
@@ -0,0 +1,196 @@
+<?php
+session_start();
+include'dbconnection.php';
+// checking session is valid for not 
+if (strlen($_SESSION['id']==0)) {
+  header('location:logout.php');
+  } else{
+
+// for deleting user
+if(isset($_GET['id']))
+{
+$adminid=$_GET['id'];
+$msg=mysqli_query($con,"delete from users where id='$adminid'");
+if($msg)
+{
+echo "<script>alert('Data deleted');</script>";
+}
+}
+?><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+
+    <title>Admin | Manage Users</title>
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/style-responsive.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+  <section id="container" >
+      <header class="header black-bg">
+              <div class="sidebar-toggle-box">
+                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+              </div>
+            <a href="#" class="logo"><b>Admin Dashboard</b></a>
+            <div class="nav notify-row" id="top_menu">
+               
+                         
+                   
+                </ul>
+            </div>
+            <div class="top-menu">
+            	<ul class="nav pull-right top-menu">
+                    <li><a class="logout" href="logout.php">Logout</a></li>
+            	</ul>
+            </div>
+        </header>
+      <aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <ul class="sidebar-menu" id="nav-accordion">
+              
+              	  <p class="centered"><a href="#"><img src="assets/img/ui-sam.jpg" class="img-circle" width="60"></a></p>
+              	  <h5 class="centered"><?php echo $_SESSION['login'];?></h5>
+              	  	
+                  <li class="mt">
+                      <a href="change-password.php">
+                          <i class="fa fa-file"></i>
+                          <span>Change Password</span>
+                      </a>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="manage-users.php" >
+                          <i class="fa fa-users"></i>
+                          <span>Manage Users</span>
+                      </a>
+                   
+                  </li>
+                  <li class="sub-menu">
+                      <a href="schedule.php" >
+                          <i class="fa fa-users"></i>
+                          <span>Schedules</span>
+                      </a>
+                   
+                  </li>
+                  <li class="sub-menu">
+                      <a href="collection.php.php" >
+                          <i class="fa fa-users"></i>
+                          <span>Collections</span>
+                      </a>
+                   
+                  </li>
+                  <li class="sub-menu">
+                      <a href="location.php" >
+                          <i class="fa fa-users"></i>
+                          <span>Locations</span>
+                      </a>
+                   
+                  </li>
+                  <li class="sub-menu">
+                      <a href="subsription.php" >
+                          <i class="fa fa-users"></i>
+                          <span>subscription</span>
+                      </a>
+                   
+                  </li>
+              
+              
+                 
+              </ul>
+          </div>
+      </aside>
+      <section id="main-content">
+          <section class="wrapper">
+          	<h3><i class="fa fa-angle-right"></i> Manage Users</h3>
+				<div class="row">
+				
+                  
+	                  
+                  <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+      <h5 class="mb-2">Info Box</h5>
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Users</span>
+                <span class="info-box-number users_count"></span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-success"><i class="far fa-flag"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Locations</span>
+                <span class="info-box-number locations_count"></span>
+              </div>
+              <!-- /.info-box-content -->
+            </div> 
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-warning"><i class="far fa-copy"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Schedules</span>
+                <span class="info-box-number schedules_count"></span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-danger"><i class="far fa-star"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Transporters</span>
+                <span class="info-box-number transporters_count"></span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+        </div>
+                      </div>
+                  </div>
+              </div>
+		</section>
+      </section
+  ></section>
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="assets/js/jquery.scrollTo.min.js"></script>
+    <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script src="assets/js/common-scripts.js"></script>
+  <script>
+      $(function(){
+          $('select.styled').customSelect();
+      });
+
+  </script>
+
+  </body>
+</html>
+<?php } ?>
